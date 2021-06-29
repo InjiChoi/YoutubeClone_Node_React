40c61e29b634a845a1ab676bbabb44d9974e1125
InjiChoi <chldlswl1212@gmail.com>
2021-06-30 05:57:05 +0900

    07. create video thumbnail with ffmpeg

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,13 +4,14 @@ const router = express.Router();
 
 const { auth } = require("../middleware/auth");
 const { multer } = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
 
 
 
-//=================================
-//             Video
-//=================================
 
+
+
+// STORAGE MULTER CONFIG
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/')
@@ -30,6 +31,7 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage }).single("file")
 
 
+
 //=================================
 //             Video
 //=================================
@@ -44,6 +46,41 @@ router.post("/uploadfiles", (req, res) => {
         return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
     })
 
+});
+
+router.post("/thumbnail", (req, res) => {
+
+    // 썸네일 생성하고 비디오 러닝타임도 가져오기
+    
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+    })
+
+    // 썸네일 생성
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3,
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
 });
 
 
